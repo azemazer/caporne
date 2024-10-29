53b23dd3d1b17500a110b6b997a7c991dda15baa
azemazer <a.laval16@laposte.net>
2024-10-29 16:50:21 +0100

    start config page nuisance + reconfig section nuisance accueil

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
 <?php
     require_once './components/header.php';
 ?>
-<div class="w-full flex flex-col items-center justify-between py-3 px-14 bg-green-200">
+<div class="w-full flex flex-col items-center justify-between py-3 px-14 bg-cover bg-[url('./resource/section-nuisance-bg.svg')]">
     <div class="flex flex-col items-center w-full px-10 py-5">
         <h3 class="text-md font-medium truncate">Nos combats</h3>
         <span class="text-sm">Héros du quotidien</span>
     </div>
     <div class="flex items-center justify-between pb-5">
         <div class="px-5 w-80">
-            <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error at, perferendis debitis nihil commodi voluptates dolorem, aspernatur obcaecati earum nostrum fuga totam ducimus incidunt nisi esse ratione eius soluta quis?</p>
+            <p class="text-sm">Notre association lutte contre la pollution et l'urbanisation qui menacent la biodiversité de la rivière. Informez-vous sur ces enjeux cruciaux pour préserver cet écosystème fragile !</p>
         </div>
         <div class="w-40">
             <a class="bg-blue-400 py-2 px-4 rounded-lg hover:text-white" href="./views/nuisance.php">Y accéder</a>
