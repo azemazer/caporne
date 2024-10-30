f2bba4b28471fed9f7f84cc3e13c46fed910fb5a
Tobias-Ketbi <tobias@ketbi.co>
2024-10-30 09:11:49 +0100

    create header

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once './components/header.php';
-require_once './components/footer.php';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti&family=Playwrite+CU:wght@100..400&display=swap"
+        rel="stylesheet">
+    <script>
+    tailwind.config = {
+        theme: {
+            colors: {
+                'vert': '#15803d',
+                'bleu': '#3970b2',
+                'jaune': '#ffff00'
+            },
+            extend: {
+                fontFamily: {
+                    kaisei: ["Kaisei Opti", serif],
+                },
+            }
+        }
+    }
+    </script>
+    <title>Cap Orne Accueil</title>
+</head>
+
+<?php require_once './components/header.php'; ?>
+
+<body>
+
+
+</body>
+
+<?php require_once './components/footer.php'; ?>
+
+</html>
