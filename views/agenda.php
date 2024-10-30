f7d28930e906b81d72d08431486633f30cd19523
T-Clement <toquet.clement@gmail.com>
2024-10-30 15:28:24 +0100

    remove dark theme handling

--- views/agenda.php
+++ views/agenda.php
@@ -82,19 +82,19 @@ require_once '../components/header.php';
         ?>
         <?php for( $i = 0; $i < 10; $i++ ) : ?>
 
-        <article class="rounded-2xl relative bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <span class="absolute right-2 top-2 bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"><?=$mutable->add($i, 'day')->isoFormat("DD/MM/YYYY")?></span>
+        <article class="rounded-2xl relative bg-white border border-gray-200 rounded-lg shadow ">
+            <span class="absolute right-2 top-2 bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded"><?=$mutable->add($i, 'day')->isoFormat("DD/MM/YYYY")?></span>
 
             <a href="#">
                 <img class="w-100 rounded-t-lg" src="./../assets/pexels-surdu-horia-743788511-29062581.jpg" alt="" />
             </a>
             <div class="p-4 relative">
                 <a href="#">
-                    <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                 <p class='flex justify-end'>
-                    <a href="#" class="relative right-1 bottom-1 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
+                    <a href="#" class="relative right-1 bottom-1 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                         En savoir +
                     </a>
 
