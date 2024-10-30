7206fb2b9b003c3fbe051fdf92b57125bb0f4472
T-Clement <toquet.clement@gmail.com>
2024-10-30 15:53:17 +0100

    fix merge issue

--- views/agenda.php
+++ views/agenda.php
@@ -1,5 +1,5 @@
 <?php require_once __DIR__ . '/../vendor/autoload.php';
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,14 +28,14 @@
     <script>
         tailwind.config = {
             theme: {
-                colors: {
-                    'vert': '#15803d',
-                    'bleu': '#3970b2',
-                    'jaune': '#ffff00'
-                },
                 extend: {
+                    colors: {
+                        'vert': '#15803d',
+                        'bleu': '#3970b2',
+                        'jaune': '#ffff00',
+                    },
                     fontFamily: {
-                        kaisei: ["Kaisei Opti", serif],
+                        kaisei: ["Kaisei Opti", "serif"],
                     },
                 }
             }
@@ -76,33 +76,33 @@ require_once '../components/header.php';
 
     <div id="events-list" class="grid grid-cols-3 gap-3">
 
-        <?php 
+        <?php
         $mutable = Carbon::now();
-        
+
         ?>
-        <?php for( $i = 0; $i < 10; $i++ ) : ?>
+        <?php for ($i = 0; $i < 10; $i++) : ?>
 
-        <article class="rounded-2xl relative bg-white border border-gray-200 rounded-lg shadow ">
-            <span class="absolute right-2 top-2 bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded"><?=$mutable->add($i, 'day')->isoFormat("DD/MM/YYYY")?></span>
+            <article class="rounded-2xl relative bg-white border border-gray-200 rounded-lg shadow ">
+                <span class="absolute right-2 top-2 bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded"><?= $mutable->add($i, 'day')->isoFormat("DD/MM/YYYY") ?></span>
 
-            <a href="#">
-                <img class="w-100 rounded-t-lg" src="./../assets/pexels-surdu-horia-743788511-29062581.jpg" alt="" />
-            </a>
-            <div class="p-4 relative">
                 <a href="#">
-                    <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                    <img class="w-100 rounded-t-lg" src="./../assets/pexels-surdu-horia-743788511-29062581.jpg" alt="" />
                 </a>
-                <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <p class='flex justify-end'>
-                    <a href="#" class="relative right-1 bottom-1 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                        En savoir +
+                <div class="p-4 relative">
+                    <a href="#">
+                        <h5 class="mb-1 text-xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
                     </a>
+                    <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p class='flex justify-end'>
+                        <a href="#" class="relative right-1 bottom-1 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                            En savoir +
+                        </a>
 
-                </p>
-            </div>
-        </article>
+                    </p>
+                </div>
+            </article>
 
-        <?php endfor ; ?>
+        <?php endfor; ?>
 
 
 
